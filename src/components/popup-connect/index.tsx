c76f9b97fb33549396695196994c7e0adc8669f4
jihyune <rlawlcns96@gmail.com>
2023-08-20 14:35:55 +0900

    feat: connect popup

---
+++ src/components/popup-connect/index.tsx
@@ -0,0 +1,102 @@
+import { useRef, useState } from 'react';
+import tw from 'twin.macro';
+import { useOnClickOutside } from 'usehooks-ts';
+
+import { COLOR } from '~/assets/colors';
+import { POPUP_ID } from '~/constants';
+import { usePopup } from '~/hooks/pages/use-popup';
+
+import { ButtonIcon } from '../buttons/button-icon';
+import { ButtonMedium } from '../buttons/button-medium';
+import { IconCancle } from '../icons';
+import { TextField } from '../textfield';
+
+export const ConnectPopup = () => {
+  const popupRef = useRef<HTMLDivElement>(null);
+  const { close } = usePopup(POPUP_ID.CONNECT);
+  useOnClickOutside(popupRef, close);
+
+  // const { setWallet, setBalance } = useWalletStore();
+  const [connectingLoading, setConnectingLoading] = useState<boolean>();
+  const [seed, setSeed] = useState<string>();
+
+  // TODO : xrp setting
+  const connect = async () => {
+    setConnectingLoading(true);
+
+    // const client = new Client(NET);
+    // await client.connect();
+    // return { client };
+  };
+
+  // const getWalletInfo = async (client: Client, wallet: Wallet) => {
+  //   const balance = await client.getXrpBalance(wallet.address);
+  //   setWallet(wallet);
+  //   setBalance(balance.toString());
+  //   client.disconnect();
+  //   close();
+  // };
+
+  // const connectWallet = async () => {
+  //   if (!seed) return;
+  //   const { client } = await connect('connect');
+  //   const wallet = Wallet.fromSeed(seed);
+  //   await getWalletInfo(client, wallet);
+  // };
+
+  return (
+    <Wrapper>
+      <PopupWrapper ref={popupRef}>
+        <ConnectWrapper>
+          <ConnectTitleWrapper>
+            <Title>Connect XRP wallet</Title>
+            <IconWrapper onClick={close}>
+              <ButtonIcon icon={<IconCancle color={COLOR.GARY3} />} />
+            </IconWrapper>
+          </ConnectTitleWrapper>
+          <TextField placeholder="Enter your seed..." onChange={e => setSeed(e.target.value)} />
+        </ConnectWrapper>
+
+        <ButtonWrapper>
+          <ButtonMedium
+            text="Connect"
+            onClick={connect}
+            isLoading={connectingLoading}
+            disabled={!seed}
+          />
+        </ButtonWrapper>
+      </PopupWrapper>
+      <Dim />
+    </Wrapper>
+  );
+};
+
+const Wrapper = tw.div``;
+
+const PopupWrapper = tw.div`
+  w-480 fixed flex flex-col bg-white z-11 absolute-center rounded-20 p-20 gap-32
+`;
+
+const ConnectWrapper = tw.div`
+  flex flex-col gap-20
+`;
+
+const ConnectTitleWrapper = tw.div`
+  flex w-full h-40 items-center justify-between
+`;
+
+const Title = tw.div`
+  font-b-18 text-purple1
+`;
+
+const IconWrapper = tw.div`
+  flex flex-center clickable
+`;
+
+const Dim = tw.div`
+  fixed w-screen h-screen bg-[#000000]/60 z-10 top-0 left-0
+`;
+
+const ButtonWrapper = tw.div`
+  flex flex-col w-full flex-center gap-16
+`;
