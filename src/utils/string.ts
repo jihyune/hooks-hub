1b9b851202cf9d0118956de883fc26383192e38a
jihyune <rlawlcns96@gmail.com>
2023-08-20 13:24:27 +0900

    feat: wallet dropdown

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,6 +1,6 @@
-export const truncateAddress = (addr?: `0x${string}`, limit = 4) => {
+export const truncateAddress = (addr?: string, limit = 4) => {
   if (!addr) return '';
 
-  const zerox = addr.slice(0, 2) == '0x' ? 2 : 0;
+  const zerox = addr.slice(0, 1) == 'r' ? 1 : 0;
   return addr.slice(0, zerox + limit) + '...' + addr.slice(-limit, addr.length);
 };
